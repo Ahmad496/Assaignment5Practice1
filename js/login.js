fc0e184056241dffd2124c59bdf4fb91a9b5d39a
Ahmad496 <sohel555582@gmail.com>
2024-12-26 20:59:21 +0600

    button added

--- js/login.js
+++ js/login.js
@@ -5,4 +5,10 @@ document
     event.preventDefault();
     console.log("login button clicked");
     const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      window.location.href = "/home.html";
+    } else {
+      alert("Wrong phone number or pin.");
+    }
   });
