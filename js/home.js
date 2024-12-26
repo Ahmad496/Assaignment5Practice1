c54e0e9b0d3bc25ecd0d2e59b312a55bcf63e243
Ahmad496 <sohel555582@gmail.com>
2024-12-26 22:05:18 +0600

    successfully money added

--- js/home.js
+++ js/home.js
@@ -5,4 +5,16 @@ document
     const addMoneyInput = document.getElementById("input-add-money").value;
     console.log(addMoneyInput);
     const pinNumberInput = document.getElementById("input-pin-number").value;
+    if (pinNumberInput === "1234") {
+      console.log("adding money to your account");
+      const balance = document.getElementById("account-balance").innerText;
+      console.log(balance);
+      const addMoneyNumber = parseFloat(addMoneyInput);
+      console.log(typeof addMoneyNumber);
+      const balanceNumber = parseFloat(balance);
+      const newBalance = addMoneyNumber + balanceNumber;
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Failed to add money! Please try again.");
+    }
   });
